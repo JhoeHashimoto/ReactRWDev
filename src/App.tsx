d21dc9c6ca6d2e19e266d7a2aa449df81efa05b6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-16 20:15:41 -0300

    4commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// @ts-check *****LINKANDO O arquivo com o CSS para extilização externa********
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
-  return (
+    const paragrafo = {
+        color: 'darkred',
+        fontSize: '2em',
+        margin: '50px'
+    }
+    return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Olá Mundo</h1>
+    <h2 style={{fontSize: '4em', color:'darkgreen'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, cupiditate.</h2>
+    
+    <p style={paragrafo}>Lorem, ipsum dolor.</p>
+
+    
     </>
-  )
+    )
 }
 
-export default App
+export default App;
